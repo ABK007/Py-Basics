a83414c6e3e3e3e6a25dea3919a0e97a2538e0cb
ABK007 <abubakarzzz07@gmail.com>
2022-10-07 23:52:05 +0500

    Added generate blanks function
    This function outputs the blanks according to the number of letters in a word.

--- Hangman.py
+++ Hangman.py
@@ -3,10 +3,14 @@ import random #imported random library
 word_list = ["Hello", "good", "delicious"] #list of words for passing as argument in random_word_selection function
 
 def random_word_selection(list):
-    """ Randomly selects a word from the words list passed as argument for guessing """
-    random_index = random.randrange(len(word_list))
+    """ Returns a word randomly selected from the words list passed as argument for guessing """
+    random_index = random.randrange(len(list))
     word = word_list[random_index]
     return word
 
-
-print (random_word_selection(word_list))
+def generate_blanks(word):
+    """ Returns blanks according to the number of letters in a word """
+    blanks = ""
+    for i in range(len(word)):
+        blanks += "_  "
+    return blanks
