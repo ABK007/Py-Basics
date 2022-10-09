bec66c13c0d6fa033ef847de3e9b7f40171b19db
ABK007 <abubakarzzz07@gmail.com>
2022-10-09 18:13:48 +0500

    defined encrypt function
    Takes input text and shift number as arguments and returns the encrypted string by shifting the letters in the input text according to the shift number.

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -4,3 +4,16 @@ alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 encodeOrDecode = input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
 input_text = input("Input your text here: \n").lower()
 shift = int(input("Type the shift number: \n"))
+
+def encrypt(text, shift):
+    encrypted_text = ""
+    for letter in text:
+        index = alphabet.index(letter)
+        shift_number = index + shift
+        encrypt_letter = alphabet[shift_number]
+        encrypted_text += encrypt_letter
+    return encrypted_text
+
+
+if encodeOrDecode == "encode":
+    print(encrypt(input_text, shift))
