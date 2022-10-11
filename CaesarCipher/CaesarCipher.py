04fc59b3925b398a891e0bd6d35aedc47efc52b2
ABK007 <abubakarzzz07@gmail.com>
2022-10-11 16:11:31 +0500

    Created main caesarCipher( ) function and added condition for numeric
    Removed the separate encrypt and decrypt function and created single function CaesarCipher() to contain all code and run this function only to run the program.
    
    Added conditions that if the element of the input_text is numeric then it is added in the Cipher string with no changes while encoding and decoding.

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -5,37 +5,45 @@ alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 
 
-print(logo)
-print("\n")
-
-encodeOrDecode = input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
-input_text = input("Input your text here: \n").lower()
-shift = int(input("Type the shift number: \n"))
-
-def encrypt(text, shift):
-    encrypted_text = ""
-    for letter in text:
-        index = alphabet.index(letter)
-        shift_number = index + shift
-        if shift_number >= 25:
-            shift_number = shift_number - 26
-        encrypt_letter = alphabet[shift_number]
-        encrypted_text += encrypt_letter
-    return encrypted_text
-
-def decrypt(text, shift):
-    encrypted_text = ""
-    for letter in text:
-        index = alphabet.index(letter)
-        shift_number = index - shift
-        if shift_number <= -1:
-            shift_number = shift_number + 26
-        encrypt_letter = alphabet[shift_number]
-        encrypted_text += encrypt_letter
-    return encrypted_text
 
 
-if encodeOrDecode == "encode":
-    print(encrypt(input_text, shift))
-elif encodeOrDecode == "decode":
-    print(decrypt(input_text, shift))
+
+def caesarCipher():
+    print(logo)
+    encodeOrDecode = input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
+    input_text = input("Input your text here: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    
+    
+
+    cipher = ""
+    if encodeOrDecode == "encode":
+        for letter in input_text:
+            if letter.isalpha() == True:
+                index = alphabet.index(letter)
+                shift_number = index + shift
+                if shift_number >= 25:
+                    shift_number = shift_number - 26
+                encrypt_letter = alphabet[shift_number]
+                cipher += encrypt_letter
+            elif letter.isnumeric() == True:
+               cipher += letter 
+        print(cipher)
+    
+    elif encodeOrDecode == "decode":
+        for letter in input_text:
+            if letter.isalpha() == True:
+                index = alphabet.index(letter)
+                shift_number = index - shift
+                if shift_number <= -1:
+                    shift_number = shift_number + 26
+                encrypt_letter = alphabet[shift_number]
+                cipher += encrypt_letter
+            elif letter.isnumeric() == True:
+                cipher += letter
+        print(cipher)
+            
+ 
+
+
+caesarCipher()
