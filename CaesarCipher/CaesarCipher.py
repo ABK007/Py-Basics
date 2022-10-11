c3a55c72f8250b9629dc01148b8f23ac9e41a373
ABK007 <abubakarzzz07@gmail.com>
2022-10-11 15:29:28 +0500

    print logo string at the start of program
    imported logo string from logo.py file and printed at the start of program

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -1,5 +1,12 @@
+from logo import logo
+
+
 alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+
+print(logo)
+print("\n")
 
 encodeOrDecode = input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
 input_text = input("Input your text here: \n").lower()
